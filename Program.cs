25fde0eed9bf55b36f03bd2b1982f1e38b214a68
nazrinmajidova <150728280+nazrinmajidova@users.noreply.github.com>
2024-05-23 13:56:09 +0400

    adding switch test

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Practice1();
+            Practice2();
+        }
+
+        public static void Practice1() //ingilis dilinde daxil edilmis fesil adinin az dilindeki qarsiligi
+        {
+            Console.Write("Write a season : ");
+            string season = Console.ReadLine().ToLower();
+
+            string movsum = season switch
+            {
+                "winter" => "qis",
+                "spring" => "yaz",
+                "summer" => "yay",
+                "autumn" => "payiz",
+                _ => "Incorrect input"
+            };
+            Console.WriteLine(movsum);
+        }
+
+        public static void Practice2()
+        {
+            Console.Write("Write a season : ");
+            string season2 = Console.ReadLine().ToLower();
+            string result = "";
+
+            switch (season2)
+            {
+                case "winter":
+                    result = "qis";
+                    break;
+                case "autumn":
+                    result = "payiz";
+                    break;
+                case "spring":
+                    result = "yaz";
+                    break;
+                case "summer":
+                    result = "yay";
+                    break;
+                default:
+                    Console.WriteLine("Incorrect input");
+                    break;
+            }
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
